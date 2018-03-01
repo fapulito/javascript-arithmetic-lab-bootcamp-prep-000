9801e0fcc6a0ee5ea646429dec56b9f793e98446
David Anderson <fpzero@gmail.com>
2018-03-01 01:01:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // solutions
 
+//toAdd
+function toAdd() {
+  return (a + b)
+}
+
+//toSubt
+function toSubt() {
+  return (a + b)
+}
+
+//toMulti
+function toMulti() {
+  return (a + b)
+}
+
+//toDiv
+function toDiv() {
+  return (a + b)
+}
+
 function toAdd() {
   return (a + b)
 }
